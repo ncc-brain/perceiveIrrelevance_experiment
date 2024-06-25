9c50be129d4c058c000e47b0ac6bd59297be7c04
EceZiya <144265169+EceZiya@users.noreply.github.com>
2024-06-25 22:23:33 +0200

    OrderEffectReady
    I finished the order effect

--- configIrrelevant.m
+++ configIrrelevant.m
@@ -13,6 +13,5 @@ faceColor = [0.8471, 0.8863, 0.8627];
 orientationColor = [1.0000, 0.90196, 0.90980];
 durationColor = [0.9882, 0.9608, 0.7804];
 
-CombinedColor = [1.0,0.706,0.635];
-
-AllCombinedColor = [0.898,0.596,0.608];
+orientationFirstColor = [1, 0.7176, 0.7647];
+durationFirstColor = [0.9451, 1, 0.7686];
